fd13cc64d8195b11d585e51e9eae2c07f24a87fc
Andrii <melnykandriy4@gmail.com>
2024-01-12 23:01:08 -0500

    first commit

---
+++ MyWeatherApp/Model/LocationModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationModel.swift
+//  MyWeatherApp
+//
+//  Created by Andrii Melnyk on 1/12/24.
+//
+
+import Foundation
---
+++ MyWeatherApp/Model/WeatherManager.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherManager.swift
+//  MyWeatherApp
+//
+//  Created by Andrii Melnyk on 1/12/24.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let myApiKey = "9e1e5b532eb06a26403bb780680a3710"
+    let cityName: String = ""
+    //let url = "https://api.openweathermap.org/data/2.5/weather?q\(cityName)&units=metric\(myApiKey)"
+    
+
+    
+    
+    
+}
